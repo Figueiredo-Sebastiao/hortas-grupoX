d49b3a4ef2c6503f0609afc9c2485a06c02c54a5
Figueiredo Sebastiao <47941@alunos.upt.pt>
2025-10-01 18:08:56 +0100

    Class Horta criada

---
+++ hortasGrupoX/src/hortasGrupoX/Horta.java
@@ -0,0 +1,21 @@
+package hortasGrupoX;
+
+public class Horta {
+
+	private String nome;
+	private String localizacao;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	
+	
+}
