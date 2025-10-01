1023ca07b3feb776c9892ad676911c5440b7a8ad
Figueiredo Sebastiao <47941@alunos.upt.pt>
2025-10-01 18:17:48 +0100

    Class pantacao criada

---
+++ hortasGrupoX/src/hortasGrupoX/Plantao.java
@@ -0,0 +1,43 @@
+package hortasGrupoX;
+
+public class Plantao {
+	
+	private String data;
+	private String sementeira;
+	private String tipoCultura;
+	
+	public Plantao(String data, String sementeira, String tipoCultura) {
+		super();
+		this.data = data;
+		this.sementeira = sementeira;
+		this.tipoCultura = tipoCultura;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getSementeira() {
+		return sementeira;
+	}
+
+	public void setSementeira(String sementeira) {
+		this.sementeira = sementeira;
+	}
+
+	public String getTipoCultura() {
+		return tipoCultura;
+	}
+
+	public void setTipoCultura(String tipoCultura) {
+		this.tipoCultura = tipoCultura;
+	}
+	
+	
+	
+	
+}
