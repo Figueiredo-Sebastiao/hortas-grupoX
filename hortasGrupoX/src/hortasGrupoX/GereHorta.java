4144995d95a9ac44f31fdbeaf7c3232711d379c4
Figueiredo Sebastiao <47941@alunos.upt.pt>
2025-10-01 18:27:29 +0100

    class gere horta criada

---
+++ hortasGrupoX/src/hortasGrupoX/GereHorta.java
@@ -0,0 +1,13 @@
+package hortasGrupoX;
+
+public class GereHorta {
+	
+	public void Registarhortas() {}
+	public void RegistarTalhõesHorta(){}
+	public void RegistarPlantaçõesTalhãoHorta(){}
+	public void ImprimirDetalhesPlantacaoHorta(){}
+	public void ImprimirAreaTotalHortaMetrosQuadrados(){}
+	public void ImprimirNomeAreHortaMaiorArea(){}
+	public void ImprimirNomeHortasTipoCultura(){}
+
+}
