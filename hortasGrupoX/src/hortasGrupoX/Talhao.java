356d28a581c8f1920184a6e28df5d3e79093e913
Figueiredo Sebastiao <47941@alunos.upt.pt>
2025-10-01 18:22:58 +0100

    class talhao criada

---
+++ hortasGrupoX/src/hortasGrupoX/Talhao.java
@@ -0,0 +1,22 @@
+package hortasGrupoX;
+
+public class Talhao {
+	
+	private int nIdentificacao;
+	private String area;
+	
+	public int getnIdentificacao() {
+		return nIdentificacao;
+	}
+	public void setnIdentificacao(int nIdentificacao) {
+		this.nIdentificacao = nIdentificacao;
+	}
+	public String getArea() {
+		return area;
+	}
+	public void setArea(String area) {
+		this.area = area;
+	}
+	
+
+}
